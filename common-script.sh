e098d91b6161850173eac33639be686a7cbe2749
Mahesh Babu Neeli <maheshbabu22.neeli@gmail.com>
2026-02-08 22:11:17 +1100

    catalogue script

--- common-script.sh
+++ common-script.sh
@@ -2,8 +2,11 @@
 
 USERID=$(id -u)                         # to find the user details
 
+MONGODB_HOST=mongodb.neeli.online
+
 LOGS_FOLDER="/var/log/shell-roboshop"   # to send the logs to a particular location/folder
 LOGS_FILE="$LOGS_FOLDER/$0.log"         # logs file name
+SCRIPT_DIR=$PWD
 
 START_TIME=$(date +%s)
 
@@ -36,4 +39,56 @@ PRINT_TOTAL_TIME(){
     END_TIME=$(date +%s)
     TOTAL_TIME=$(( $END_TIME - $START_TIME ))
     echo -e "$(date "+%Y-%m-%d %H:%M:%S") | Script execute in: $G $TOTAL_TIME seconds $N" | tee -a $LOGS_FILE
+}
+
+APP_SETUP() {
+    id roboshop &>>$LOGS_FILE
+    if [ $? -ne 0 ]; then
+        useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOGS_FILE
+        VALIDATE $? "Creating system user"
+    else
+        echo -e "Roboshop user already exist ... $Y SKIPPING $N"
+    fi
+
+    # downloading the app
+    mkdir -p /app
+    VALIDATE $? "Creating app directory"
+
+    curl -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip  &>>$LOGS_FILE
+    VALIDATE $? "Downloading $app_name code"
+
+    cd /app
+    VALIDATE $? "Moving to app directory"
+
+    rm -rf /app/*
+    VALIDATE $? "Removing existing code"
+
+    ls -la
+
+    unzip /tmp/$app_name.zip &>>$LOGS_FILE
+    VALIDATE $? "Uzip $app_name code"
+}
+
+NODEJS_SETUP() {
+    dnf module disable nodejs -y &>>$LOGS_FILE
+    VALIDATE $? "Disabling NodeJS Default version"
+
+    dnf module enable nodejs:20 -y &>>$LOGS_FILE
+    VALIDATE $? "Enabling NodeJS 20"
+
+    dnf install nodejs -y &>>$LOGS_FILE
+    VALIDATE $? "Install NodeJS"
+
+    npm install  &>>$LOGS_FILE
+    VALIDATE $? "Installing dependencies"
+}
+
+SYSTEMD_SETUP() {
+    cp $SCRIPT_DIR/$APP_NAME.service /etc/systemd/system/$APP_NAME.service
+    VALIDATE $? "Created systemctl service"
+
+    systemctl daemon-reload
+    systemctl enable $APP_NAME  &>>$LOGS_FILE
+    systemctl start $APP_NAME
+    VALIDATE $? "Starting and enabling $APP_NAME"
 }
